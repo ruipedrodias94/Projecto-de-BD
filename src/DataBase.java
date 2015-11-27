538dba4269e0919a515a7bda1093729f7c2e1bc5
Rui Pedro Dias <ruypedrodias94@gmail.com>
2015-11-27 18:42:03 +0000

    Funções de SQL
    Falta:
    - Consultar Recompensa
    - Doar dinheiro a um projecto
    - Adicionar ou remover uma recompensa ao projecto
    - Cancelar projecto
    - Responder a mensagens de apoiantes
    - Finalizar projecto

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Created by Rui Dias on 27/11/2015.
@@ -52,6 +53,22 @@ public class DataBase {
 
     //Funcoes de base de dados
 
+    //Obter o id do cliente logado
+    public synchronized int getIdCliente(String username) throws SQLException{
+        int id = 0;
+        try{
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM proj_bd.cliente WHERE user_Name = '" + username + "';");
+            while (resultSet.next()){
+                id = resultSet.getInt(1);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return id;
+    }
+
+    //Registar conta
     public synchronized void registarConta(String nome_Cliente, String user_Name, String password, int saldo) throws SQLException{
 
         try{
@@ -72,4 +89,140 @@ public class DataBase {
             e.printStackTrace();
         }
     }
+
+    //Login
+    public synchronized boolean login(String user_Name, String password) throws SQLException{
+        try{
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM proj_bd.cliente" +
+                    " WHERE user_Name = '"+user_Name +"' AND password = '"+ password +"';");
+
+            //Se obtiver mais que um resultado
+            while (resultSet.next()){
+                return true;
+            }
+        }catch (SQLException e){
+            System.out.println(e.getLocalizedMessage());
+        }
+
+        //Caso contrario retorna falso
+        return false;
+    }
+
+    //Listar projectos actuais
+    public synchronized ArrayList<String> listarProjectos(int state) throws SQLException{
+        ArrayList<String> projectos_Actuais = new ArrayList<>();
+        String nome_Projecto, data_Final, estado = "";
+        String string_Final = "";
+        int id_Projecto = 0;
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT nome_Projecto, data_Limite, idProjecto FROM proj_bd.projecto" +
+                    " WHERE projecto.estado ="+ state +";");
+            while (resultSet.next()){
+                nome_Projecto = resultSet.getString(1);
+                data_Final = resultSet.getString(2);
+                id_Projecto = resultSet.getInt(3);
+                if(state == 1) {
+                    estado = "ACTIVO";
+                } else {
+                    estado = "INACTIVO";
+                }
+                string_Final = "NOME DO PROJECTO: " + nome_Projecto +"\nID DO PROJECTO: "+ String.valueOf(id_Projecto) +"\nDATA LIMITE: " + data_Final +
+                        "\nESTADO: " + estado + "\n====================================================";
+
+                projectos_Actuais.add(string_Final);
+            }
+        }catch (SQLException e){
+            System.out.println(e.getLocalizedMessage());
+        }
+
+        return projectos_Actuais;
+    }
+
+    //Consultar o saldo
+    public synchronized int consultarSaldo(int id_Cliente) throws SQLException{
+
+        int saldo_Cliente = 0;
+        try{
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT saldo FROM proj_bd.cliente WHERE idCliente =" + id_Cliente+";");
+
+            while (resultSet.next()){
+                saldo_Cliente = resultSet.getInt(1);
+            }
+        }catch (SQLException e){
+            System.out.println(e.getLocalizedMessage());
+        }
+        return saldo_Cliente;
+    }
+
+    //Criar um projecto
+    public synchronized void criarProjecto(String nome_Projecto, String desricao_Projecto, String data,
+                                           int id_Cliente, int dinheiro_Limite ) throws  SQLException{
+        //Saldo = 0
+        //Estado = 1 ---> Activo
+        //Data limite ---> Ainda nao sei
+        //Dinheiro angariado = 0
+        //id_Cliente ---> Tambem ainda nao sei como meter
+        //A data vai ter que ser do tipo 20151030  ---> 30-10-2015 Passamos como string, e no menu pede-se o dia o mes e o ano, tornando dempois numa string
+
+
+        try{
+            preparedStatement = connection.prepareStatement("INSERT INTO proj_bd.projecto (nome_Projecto, descricao_Projecto, estado, data_Limite," +
+                    " dinheiro_Angariado, dinheiro_Limite, Cliente_idCliente) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?);");
+
+            preparedStatement.setString(1,nome_Projecto);
+            preparedStatement.setString(2,desricao_Projecto);
+            preparedStatement.setInt(3, 1);
+            preparedStatement.setDate(4, Date.valueOf(data));
+            preparedStatement.setInt(5, 0);
+            preparedStatement.setInt(6,dinheiro_Limite);
+            preparedStatement.setInt(7, id_Cliente);
+
+            preparedStatement.executeUpdate();
+
+            System.out.println("PROJECTO CRIADO COM SUSEXO");
+
+        }catch (SQLException e){
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+
+    //Listar detalhes do projecto
+    public synchronized String listarDetalhes_Projecto(int id_Projecto) throws SQLException{
+        String string_Final = "", nome_Projecto = "", descricao_Projecto= "";
+        int dinheiro_Angariado = 0, dinheiro_Limite = 0;
+        Date data_Limite = null;
+
+        try{
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT nome_Projecto, descricao_Projecto, data_Limite, dinheiro_Angariado, dinheiro_Limite" +
+                    " FROM proj_bd.projecto WHERE idProjecto= " + id_Projecto + ";");
+            if(resultSet.next()){
+                nome_Projecto = resultSet.getString(1);
+                descricao_Projecto = resultSet.getString(2);
+                data_Limite = resultSet.getDate(3);
+                dinheiro_Angariado = resultSet.getInt(4);
+                dinheiro_Limite = resultSet.getInt(5);
+
+                string_Final = "DETALHES DO PROJECTO: \n"
+                        + "NOME: " + nome_Projecto + "\n"
+                        + "DESCRICAO: " + descricao_Projecto + "\n"
+                        + "DATA LIMITE: " + String.valueOf(data_Limite) + "\n"
+                        + "DINHEIRO ANGARIADO: " + String.valueOf(dinheiro_Angariado) + "\n"
+                        + "DINHEIRO NECESSARIO: " + String.valueOf(dinheiro_Limite)
+                        + "\n====================================================";
+
+            }
+        }catch (SQLException e){
+            System.out.println(e.getLocalizedMessage());
+        }
+
+        return string_Final;
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,60 @@
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+
+
+    public static void main(String[] args) throws SQLException, ParseException {
+
+        //Variaveis
+        boolean login = false;
+        int id_Cliente = 0;
+        ArrayList<String> projectos = new ArrayList<>();
+        int saldo = 0;
+        String detalhes = "";
+
 
         //Esta vai ser a nossa interface!
         DataBase dataBase = new DataBase();
 
+
         //Registar Cliente
-        dataBase.registarConta("Rui Pedro", "ruipedrodias", "12345", 10000);
+        //dataBase.registarConta("Rui Pedro", "ruipedrodias", "12345", 10000);
+
+        //Login
+        //login = dataBase.login("ruipedrodias", "12345");
+
+        //Get id_Cliente
+        //id_Cliente = dataBase.getIdCliente("ruipedrodias");
+        //System.out.println(id_Cliente);
+
+        //Listar os projectos, temos de ter uma variavel para o estado  activo --- 1 inactivo -- 0;
+        //projectos = dataBase.listarProjectos(1);
+
+        //Consultar saldo
+        //saldo = dataBase.consultarSaldo(1);
+        //System.out.print(saldo);
+
+        //Criar projecto
+        //dataBase.criarProjecto("Primeiro","Isto é um teste", "2015-11-27", 1, 10000);
+
+        /*String a = "2015-11-27";
+        String b = "2015-11-28";
+
+        DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+        Date date1 = format.parse(a);
+        Date date2 = format.parse(b);
+
+        if (date2.before(date1)){
+            System.out.println("A data dois e mais velha que a um");
+        }*/
+
+        //detalhes = dataBase.listarDetalhes_Projecto(1);
+        //System.out.println(detalhes);
     }
 }
